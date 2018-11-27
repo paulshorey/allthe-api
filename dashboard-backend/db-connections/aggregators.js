16a449fae46d80f47b6a498544e100b2de3fbc0b
Paul Shorey <pshorey@beyond.ai>
2018-11-27 09:40:40 -0800

    mongoose connections insert find working ~ next is to test over REST HTTP

--- dashboard-backend/db-connections/aggregators.js
+++
@@ -1,7 +0,0 @@
-const DB_NAME = "aggregators";
-const SHH = require("../../../www-node-secrets.js");
-
-// connect
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://" + SHH.mongodb[DB_NAME].user + ":" + SHH.mongodb[DB_NAME].pwd + "@localhost/" + DB_NAME,{ useNewUrlParser: true }).then(function() {});
-module.exports = exports = mongoose;
